ed71656a07bf2d76fe4e6e15cd67356fbc292bc9
JYCabello <j.cabello.ramos@gmail.com>
2021-12-24 11:16:48 +0100

    Moved some code

--- FpIntroWebAPI/FpIntroWebAPI/Controllers/WeatherForecastController.cs
+++ FpIntroWebAPI/FpIntroWebAPI/Controllers/WeatherForecastController.cs
@@ -7,45 +7,6 @@ using static DeFuncto.Prelude;
 
 namespace FpIntroWebAPI.Controllers;
 
-public class MyError
-{
-    public readonly Du5<KeyMissing, KeyInvalid, UserInactive, PermissionMissing, EntityNotFound> Value;
-    public MyError(Du5<KeyMissing, KeyInvalid, UserInactive, PermissionMissing, EntityNotFound> value) =>
-        Value = value;
-
-    public static MyError KeyMissing => new(new KeyMissing());
-    public static MyError KeyInvalid => new(new KeyInvalid());
-    public static MyError UserInactive(string username) => new(new UserInactive(username));
-    public static MyError PermissionMissing(string username, string role) => new(new PermissionMissing(username, role));
-    public static MyError EntityNotFound(string message) => new(new EntityNotFound(message));
-}
-
-public class KeyMissing { }
-public class KeyInvalid { }
-
-public class UserInactive
-{
-    public UserInactive(string username) =>
-        Username = username;
-    public string Username { get; }
-}
-public class PermissionMissing
-{
-    public PermissionMissing(string username, string role)
-    {
-        Username = username;
-        Role = role;
-    }
-    public string Username { get; }
-    public string Role { get; }
-}
-
-public class EntityNotFound
-{
-    public EntityNotFound(string message) =>
-        Message = message;
-    public string Message { get; }
-}
 
 public class MyData {}
 
@@ -54,10 +15,6 @@ public record User2(bool IsActive, string Username)
     public bool HasRole(string roleName) => true;
 }
 
-public static class Meh
-{
-
-}
 
 [ApiController]
 [Route("[controller]")]
